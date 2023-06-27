e97dc828cecccd23e7f5e227cbba9e9cf42434db
rohitbairwa369 <rohitbairwa36910@gmail.com>
2023-06-27 21:13:01 +0530

    improvement on feedback

--- src/app/feature/running-quiz/running-quiz.component.ts
+++ src/app/feature/running-quiz/running-quiz.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-running-quiz',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RunningQuizComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(@Inject(DOCUMENT) private document: any) {}
+  elem: any;
+
+  ngOnInit() {
+    this.elem = document.documentElement;
+    this.openFullscreen()
+  }
+
+  openFullscreen() {
+    if (this.elem.requestFullscreen) {
+      this.elem.requestFullscreen();
+    } else if (this.elem.mozRequestFullScreen) {
+      /* Firefox */
+      this.elem.mozRequestFullScreen();
+    } else if (this.elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.elem.webkitRequestFullscreen();
+    } else if (this.elem.msRequestFullscreen) {
+      /* IE/Edge */
+      this.elem.msRequestFullscreen();
+    }
+  }
+
+  /* Close fullscreen */
+  closeFullscreen() {
+    if (this.document.exitFullscreen) {
+      this.document.exitFullscreen();
+    } else if (this.document.mozCancelFullScreen) {
+      /* Firefox */
+      this.document.mozCancelFullScreen();
+    } else if (this.document.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.document.webkitExitFullscreen();
+    } else if (this.document.msExitFullscreen) {
+      /* IE/Edge */
+      this.document.msExitFullscreen();
+    }
   }
 
 }
