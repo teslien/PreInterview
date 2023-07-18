f4ebfab0bc9199f5412bc25d6416a55a7903c2ab
rohitbairwa369 <rohitbairwa36910@gmail.com>
2023-07-18 20:14:49 +0530

    svg,table,route ,service

--- src/app/feature/admin/report-card/report-card.component.ts
+++ src/app/feature/admin/report-card/report-card.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { TestDataService } from 'src/app/service/test-data.service';
 
 @Component({
   selector: 'app-report-card',
   templateUrl: './report-card.component.html',
   styleUrls: ['./report-card.component.scss']
 })
-export class ReportCardComponent implements OnInit {
+export class ReportCardComponent implements OnInit,OnDestroy {
 
-  constructor() { }
+  constructor(private testService: TestDataService,private activatedRoute: ActivatedRoute) { }
+  ngOnDestroy(): void {
+    this.dataSubscription.unsubscribe();
+  }
   value=100;
+  reportCard:any;
+  adminId=localStorage.getItem('UserId');
+  dataSubscription:Subscription;
+
   ngOnInit(): void {
+    this.dataSubscription= this.activatedRoute.queryParams.subscribe(data=>{
+      const obj = data;
+      this.testService.getSpecificApplicantdata(obj['id'],this.adminId).subscribe((res)=>{
+        this.reportCard=res;
+        console.log(this.reportCard);
+      })
+    })
   }
 
 }
